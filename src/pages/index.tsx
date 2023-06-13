f85f786f483ed8aef2fa5a4332dddf18a88fdfac
seungmin-dev <seungmin.dev@gmail.com>
2023-06-13 14:56:59 +0900

    홈화면 다이어리 링크 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "@/components/layout";
 import { playlist } from "@/data/playlist";
 import { GET_LIST } from "./api/query";
 import { useQuery } from "@apollo/client";
+import Link from "next/link";
 
 export default function Home() {
   const { data } = useQuery(GET_LIST);
@@ -17,14 +18,18 @@ export default function Home() {
             <div>
               {data.fetchBoards
                 .slice(0, 4)
-                .map((post: { title: string }, index: number) => (
-                  <p key={index} className="text-sm mb-1">
-                    <span className="p-1 bg-[#EE3E61] rounded-md text-white text-xs mr-1">
-                      다이어리
-                    </span>
-                    {post.title}
-                  </p>
-                ))}
+                .map(
+                  (post: { title: string; number: number }, index: number) => (
+                    <Link href={`/diary/${post.number}`} key={index}>
+                      <p className="text-sm mb-1">
+                        <span className="p-1 bg-[#EE3E61] rounded-md text-white text-xs mr-1">
+                          다이어리
+                        </span>
+                        {post.title}
+                      </p>
+                    </Link>
+                  )
+                )}
             </div>
           </div>
           <div className="w-2/5">
