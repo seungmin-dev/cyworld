e85d09a2749c5042bd24cdca38b564890272925e
seungmin-dev <seungmin.dev@gmail.com>
2023-06-14 13:05:07 +0900

    다이어리 페칭 버그 수정

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://practice.codebootcamp.co.kr/graphql/",
+  uri: "https://practice.codebootcamp.co.kr/graphql/",
   cache: new InMemoryCache(),
 });
 
